7ac4a72e490dadc59f4c9952a3095d4deeccc26f
Luciano <britoluci410@gmail.com>
2023-11-26 18:48:39 -0300

    Cambios realizados para la activadad 16 *Agregar css, detalle.js y detalle.php

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-// Evento que se ejecuta cuando se carga completamente la pagina
+// Evento que se ejecuta cuando se carga completamente la pÃ¡gina
 document.addEventListener("DOMContentLoaded", () => {
-
-	// pedimos las estaciones
-	loadEstaciones().then( data => {
-
-		// recorremos el listado de estaciones
-		data.forEach(function(element, index){
-
-			// creamos los botones de estaciones
-			addBtnEstacion(element)
-		})
-	})
+    // pedimos las estaciones
+    loadEstaciones().then( data => {
+        // recorremos el listado de estaciones
+        data.forEach(function(element, index){
+            // creamos los botones de estaciones
+            addBtnEstacion(element)
+        })
+    })
 })
-
-// Peticion asincrona de la lista de estaciones
+// PeticiÃ³n asincrona de la lista de estaciones
 async function loadEstaciones(){
-	const response = await fetch("https://mattprofe.com.ar/proyectos/app-estacion/datos.php?mode=list-stations")
-	const data = await response.json()
-
-	return data
+    const response = await fetch("https://mattprofe.com.ar/proyectos/app-estacion/datos.php?mode=list-stations")
+    const data = await response.json()
+    return data
 }
-
-// Crea un nuevo boton con los datos de info
+// Crea un nuevo botÃ³n con los datos de info
 function addBtnEstacion(info){
+    let tpl = document.querySelector("#tpl-btn-estacion");
+    let clon = tpl.content.cloneNode(true);
 
-	let tpl = document.querySelector("#tpl-btn-estacion");
-	let clon = tpl.content.cloneNode(true);
+    console.log(info);
 
-	console.log(info);
+    // cargamos los datos del botÃ³n clonado
+    clon.querySelector(".btn-estacion").setAttribute("href", "./detalle.php?chipid="+info.chipid);
+    clon.querySelector(".estacion-ubicacion").innerHTML= '<i class="rojo material-symbols-outlined">location_on</i>'+'<h3>'+info.ubicacion+'<h3>';
+    clon.querySelector(".estacion-visitas").innerHTML = '<h3>'+info.visitas+'</h3>'+'<i class="rojo material-symbols-outlined">rss_feed</i>'
+    clon.querySelector(".estacion-apodo").innerHTML = '<h2>'+info.apodo+'</h2>';
 
-	// cargamos los datos del boton clonado
-	clon.querySelector(".btn-estacion").setAttribute("href", "./detalle/"+info.chipid);
-	clon.querySelector(".estacion-ubicacion").innerHTML= '<i class="rojo material-symbols-outlined">location_on</i>'+'<h3>'+info.ubicacion+'<h3>';
-	clon.querySelector(".estacion-visitas").innerHTML = '<i class="rojo material-symbols-outlined">rss_feed</i>'+'<h3>'+info.visitas+'</h3>'
-	clon.querySelector(".estacion-apodo").innerHTML = '<h2>'+info.apodo+'</h2>';
-	
-	// Agrega un nuevo boton de estacion
-	document.querySelector("#list-estacion").appendChild(clon)
-}
+// Agrega un nuevo botÃ³n de estaciÃ³n
+    document.querySelector("#list-estacion").appendChild(clon)
+} 
